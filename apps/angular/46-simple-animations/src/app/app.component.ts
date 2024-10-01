35842fbcc4a324c1f05097b8eab8ce32c87a0683
jellespruyt <bnchy@proton.me>
2024-10-01 10:58:19 +0200

    feat: answer 46

--- apps/angular/46-simple-animations/src/app/app.component.ts
+++ apps/angular/46-simple-animations/src/app/app.component.ts
@@ -1,9 +1,46 @@
+import {
+  animate,
+  query,
+  stagger,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   standalone: true,
   imports: [],
   selector: 'app-root',
+  animations: [
+    trigger('fadeInAnimation', [
+      transition(':enter', [
+        style({ transform: 'translateX(-50px)', opacity: 0 }),
+        animate(
+          '500ms ease-in',
+          style({ transform: 'translateX(0)', opacity: 1 }),
+        ),
+      ]),
+    ]),
+    trigger('staggerAnimation', [
+      transition(':enter', [
+        query('.list-item', [style({ opacity: 0 })]),
+        query(
+          '.list-item',
+          [
+            stagger('200ms', [
+              style({ transform: 'translateX(-50px)', opacity: 0 }),
+              animate(
+                '500ms ease-in',
+                style({ transform: 'translateX(0)', opacity: 1 }),
+              ),
+            ]),
+          ],
+          { optional: true },
+        ),
+      ]),
+    ]),
+  ],
   styles: `
     section {
       @apply flex flex-1 flex-col gap-5;
@@ -19,7 +56,7 @@ import { Component } from '@angular/core';
   `,
   template: `
     <div class="mx-20 my-40 flex gap-5">
-      <section>
+      <section [@fadeInAnimation]>
         <div>
           <h3>2008</h3>
           <p>
@@ -51,7 +88,7 @@ import { Component } from '@angular/core';
         </div>
       </section>
 
-      <section>
+      <section [@staggerAnimation]>
         <div class="list-item">
           <span>Name:</span>
           <span>Samuel</span>
